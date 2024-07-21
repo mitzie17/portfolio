30ea6a0970d8b691c739a4c6d493ad2ee10080a3
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-07-20 22:41:12 -0700

    import a few major components and create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 
 import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages/Home";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
     <main className="flex h-screen">
-      Hello world!
       <Routes>
         {/* public routes */}
-        <Route path="/sign-in" element={<SigninForm />}/>
-        <Route index element={<Home />}
+        <Route element={<AuthLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
         {/* private routes */}
+        <Route element={<RootLayout />}>
+          {/* NOTE: The Home route could be place here?? */}
+        </Route>
       </Routes>
     </main>
   );
