3ff2ed385d350a3c3c3a49fd9abda44abb64e2ce
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-08-05 21:55:56 -0700

    configure all collections in appwrite

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,8 +1,14 @@
 import { Client, Account, Databases, Storage, Avatars } from "appwrite";
 
 export const appwriteConfig = {
-  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   url: import.meta.env.VITE_APPWRITE_URL,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  projectCollectionId: import.meta.env.VITE_APPWRITE_PROJECT_COLLECTION_ID,
+  workCollectionId: import.meta.env.VITE_APPWRITE_WORK_COLLECTION_ID,
+  skillCollectionId: import.meta.env.VITE_APPWRITE_SKILL_COLLECTION_ID,
 };
 
 export const client = new Client();
