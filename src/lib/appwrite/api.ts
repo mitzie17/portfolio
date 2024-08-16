cb6471dbefa22f747d834a0cef9df716b755aeee
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-08-16 10:19:19 -0700

    define signInAccount() which creates a session from appwrite

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -48,3 +48,16 @@ export async function saveUserToDB(user: {
     console.log(error);
   }
 }
+
+export async function signInAccount(user: { email: string; password: string }) {
+  try {
+    const session = await account.createEmailPasswordSession(
+      user.email,
+      user.password
+    );
+
+    return session;
+  } catch (error) {
+    console.log(error);
+  }
+}
