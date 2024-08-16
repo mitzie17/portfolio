4b7ac7f47c45449331eabe887c7eb56d4ed1250e
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-08-16 10:17:25 -0700

    login user into a session inside submit handler, utilize mutation functions to create an account and sign in the user

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -16,11 +16,20 @@ import { SignupValidationSchema } from "@/lib/validation";
 import { z } from "zod";
 import Loader from "@/components/shared/Loader";
 import { Link } from "react-router-dom";
-import { createUserAccount } from "@/lib/appwrite/api";
+
+import {
+  useCreateUserAccountMutation,
+  useSignInAccountMutation,
+} from "@/lib/react-query/queriesAndMutations";
 
 const SignupForm = () => {
   const { toast } = useToast();
-  const isLoading = false;
+
+  const { mutateAsync: createUserAccount, isLoading: isCreatingUser } =
+    useCreateUserAccountMutation();
+
+  const { mutateAsync: signInAccount, isLoading: isSigningIn } =
+    useSignInAccountMutation();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidationSchema>>({
@@ -41,6 +50,15 @@ const SignupForm = () => {
 
     if (!newUser) {
       return toast({ title: "Sign up failed. Please try again." });
+    }
+
+    const session = await signInAccount({
+      email: values.email,
+      password: values.password,
+    });
+
+    if (!session) {
+      return toast({ title: "Sign in failed. Please try again." });
     }
   }
 
@@ -110,7 +128,7 @@ const SignupForm = () => {
             )}
           />
           <Button type="submit" className="shad-button_primary">
-            {isLoading ? (
+            {isCreatingUser ? (
               <div className="flex-center gap-2">
                 <Loader /> Loading...
               </div>
