b1d02b13b9449e1712f474033e48cb5defb09006
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-08-22 21:45:52 -0700

    create left side bar with links

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,5 +1,50 @@
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
+import { useSignOutAccountMutation } from "@/lib/react-query/queriesAndMutations";
+import { useEffect } from "react";
+import { useUserContext } from "@/context/AuthContext";
+import { sidebarLinks } from "@/constants";
+import { INavLink } from "@/types";
+
 const LeftSidebar = () => {
-  return <div>LeftSidebar</div>;
+  const { mutate: signOut, isSuccess } = useSignOutAccountMutation();
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
+  return (
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex gap-3 items-center">
+          <h1>My Profile</h1>
+        </Link>
+
+        <Link to={`/profile/${user.id}`} className="flex gap-3 items-center">
+          <img
+            src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+            alt="profile"
+            className="h-14 w-14 rounded-full"
+          />
+          <div className="flex flex-col">
+            <p className="body-bold">{user.name}</p>
+            <p className="small-regular text-light-3">@{user.username}</p>
+          </div>
+        </Link>
+
+        <ul className="flex flex-col gap-6">
+          {sidebarLinks.map((link: INavLink) => {
+            return (
+              <li key={link.label} className="leftsidebar-link">
+                <NavLink to={link.route}>{link.label}</NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default LeftSidebar;
