483f151a08164fce115322c8c19586800ceddef5
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-08-26 21:25:30 -0700

    add heading in CreateProject component

--- src/_root/pages/CreateProject.tsx
+++ src/_root/pages/CreateProject.tsx
@@ -1,5 +1,25 @@
+import ProjectForm from "@/components/forms/ProjectForm";
+
 const CreateProject = () => {
-  return <div>CreateProject</div>;
+  return (
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="max-w-5xl flex-start gap-3 justify-start w-full">
+          <img
+            src="/assets/icons/add-post.svg"
+            width={36}
+            height={36}
+            alt="add"
+          />
+          <h2 className="h3-bold md:h2-bold text-left w-full">
+            Create Project
+          </h2>
+        </div>
+
+        <ProjectForm />
+      </div>
+    </div>
+  );
 };
 
 export default CreateProject;
