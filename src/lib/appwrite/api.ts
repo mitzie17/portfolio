a82d5f09b8adc7e533e13c38b7fb0bc1efab6d77
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-08-30 22:21:04 -0700

    define createProject and uploadFile

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,5 +1,5 @@
-import { INewUser } from "@/types";
-import { account, appwriteConfig, avatars, databases } from "./config";
+import { INewProject, INewUser } from "@/types";
+import { account, appwriteConfig, avatars, databases, storage } from "./config";
 import { ID, Query } from "appwrite";
 
 export async function createUserAccount(user: INewUser) {
@@ -98,6 +98,32 @@ export async function signOutAccount() {
     const session = await account.deleteSession("current");
 
     return session;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function createProject(project: INewProject) {
+  try {
+    // Upload image to storage
+    const uploadedFile = await uploadFile(project.file[0]);
+
+    if (!uploadedFile) throw Error;
+
+    // Get file url
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function uploadFile(file: File) {
+  try {
+    const uploadedFile = await storage.createFile(
+      appwriteConfig.storageId,
+      ID.unique(),
+      file
+    );
+    return uploadedFile;
   } catch (error) {
     console.log(error);
   }
