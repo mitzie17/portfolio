24ada0e6b18aeb66fd49b015ffb35142250600ce
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-08-31 22:20:39 -0700

    create enum for keys that will invalidate specific queries, avoids typos and bugs

---
+++ src/lib/react-query/queryKeys.ts
@@ -0,0 +1,23 @@
+// File helps avoid mistyping terms
+// These keys are used to invalidate specific queries so the data that is fetched
+// is the most updated and not from the already cached data
+export enum QUERY_KEYS {
+  // AUTH KEYS
+  CREATE_USER_ACCOUNT = "createUserAccount",
+
+  // USER KEYS
+  GET_CURRENT_USER = "getCurrentUser",
+  GET_USERS = "getUsers",
+  GET_USER_BY_ID = "getUserById",
+
+  // POST KEYS
+  GET_PROJECTS = "getProjects",
+  GET_INFINITE_PROJECTS = "getInfiniteProjects",
+  GET_RECENT_PROJECTS = "getRecentProjects",
+  GET_PROJECT_BY_ID = "getProjectById",
+  GET_USER_PROJECTS = "getUserProjects",
+  GET_FILE_PREVIEW = "getFilePreview",
+
+  //  SEARCH KEYS
+  SEARCH_PROJECTS = "getSearchProjects",
+}
