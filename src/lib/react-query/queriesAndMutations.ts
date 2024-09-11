d6a1a76527272bb8f491b86e6837a9156899c848
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-09-10 22:58:00 -0700

    define useLikeProject

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -8,11 +8,13 @@ import {
   createProject,
   createUserAccount,
   getRecentProjects,
+  likeProject,
   signInAccount,
   signOutAccount,
 } from "../appwrite/api";
 import { INewProject, INewUser } from "@/types";
 import { QUERY_KEYS } from "./queryKeys";
+import { LucideArrowUpRightFromSquare } from "lucide-react";
 
 export const useCreateUserAccountMutation = () => {
   return useMutation({
@@ -49,5 +51,33 @@ export const useGetRecentProjectsMutation = () => {
   return useQuery({
     queryKey: [QUERY_KEYS.GET_RECENT_PROJECTS],
     queryFn: getRecentProjects,
+  });
+};
+
+export const useLikeProject = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      projectId,
+      likesArray,
+    }: {
+      projectId: string;
+      likesArray: string[];
+    }) => likeProject(projectId, likesArray),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_PROJECT_BY_ID, data?.$id],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_PROJECTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_PROJECTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+      });
+    },
   });
 };
