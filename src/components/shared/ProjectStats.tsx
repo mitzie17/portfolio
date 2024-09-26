764ae3b2cb7feaabded65dcc5e8544ddb7fe6d66
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-09-25 22:06:14 -0700

    define handleLikeProject

--- src/components/shared/ProjectStats.tsx
+++ src/components/shared/ProjectStats.tsx
@@ -1,33 +1,55 @@
-import { useUserContext } from "@/context/AuthContext";
-import { useLikeProject } from "@/lib/react-query/queriesAndMutations";
+import { useLikeProjectMutation } from "@/lib/react-query/queriesAndMutations";
+import { checkIsLiked } from "@/lib/utils";
 import { Models } from "appwrite";
 import { useState, useEffect } from "react";
 
 type ProjectStatsProps = {
-  project: Models.Document;
+  project?: Models.Document;
   userId: string;
 };
 
 const ProjectStats = ({ project, userId }: ProjectStatsProps) => {
-  const likesList = project.likes.map((user: Models.Document) => user.$id);
+  const likesList = project?.likes.map((user: Models.Document) => user.$id);
+
   const [likes, setLikes] = useState(likesList);
 
-  const { mutate: likeProject } = useLikeProject();
+  const { mutate: likeProject } = useLikeProjectMutation();
 
-  const { data: currentUser } = useUserContext();
+  //const { data: currentUser } = useGetCurrentUser();
+
+  const handleLikeProject = (e: React.MouseEvent) => {
+    e.stopPropagation();
+
+    let newLikes = [...likes];
+
+    const hasLiked = newLikes.includes(userId);
+
+    if (hasLiked) {
+      newLikes = newLikes.filter((id) => id !== userId);
+    } else {
+      newLikes.push(userId);
+    }
+
+    setLikes(newLikes);
+    likeProject({ projectId: project?.$id || "", likesArray: newLikes });
+  };
 
   return (
     <div className="flex justify-between items-center z-20">
       <div className="flex gap-2 ">
         <img
-          src="/assets/icons/like.svg"
+          src={`${
+            checkIsLiked(likes, userId)
+              ? "/assets/icons/liked.svg"
+              : "/assets/icons/like.svg"
+          }`}
           alt="like"
           width={20}
           height={20}
-          onClick={() => {}}
+          onClick={handleLikeProject}
           className="cursor-pointer"
         />
-        <p className="small-medium lg:base-medium">80</p>
+        <p className="small-medium lg:base-medium">{likes.length}</p>
       </div>
     </div>
   );
