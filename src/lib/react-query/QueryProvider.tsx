4dec3bee5c2165fa171676b8c56e67d25a7a47b0
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-09-25 22:10:38 -0700

    change order of imports

--- src/lib/react-query/QueryProvider.tsx
+++ src/lib/react-query/QueryProvider.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
 
 const queryClient = new QueryClient();
 
