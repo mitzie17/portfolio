046cb6a645498a814935da16e064a7a71666ca7b
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-09-27 21:38:41 -0700

    import IUpdateProject, useGetProjectById, useUpdateProjectMutation, and useDeleteProject

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,13 +1,16 @@
-import { INewProject, INewUser } from "@/types";
+import { INewProject, INewUser, IUpdateProject } from "@/types";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import {
   createProject,
   createUserAccount,
+  deleteProject,
   getCurrentUser,
+  getProjectById,
   getRecentProjects,
   likeProject,
   signInAccount,
   signOutAccount,
+  updateProject,
 } from "../appwrite/api";
 import { QUERY_KEYS } from "./queryKeys";
 
@@ -81,5 +84,43 @@ export const useGetCurrentUser = () => {
   return useQuery({
     queryKey: [QUERY_KEYS.GET_CURRENT_USER],
     queryFn: getCurrentUser,
+  });
+};
+
+export const useGetProjectById = (projectId?: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_PROJECT_BY_ID, projectId],
+    queryFn: () => getProjectById(projectId),
+    enabled: !!projectId,
+  });
+};
+
+export const useUpdateProjectMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (project: IUpdateProject) => updateProject(project),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_PROJECT_BY_ID, data?.$id],
+      });
+    },
+  });
+};
+
+export const useDeleteProject = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({
+      projectId,
+      imageId,
+    }: {
+      projectId?: string;
+      imageId: string;
+    }) => deleteProject(projectId, imageId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_PROJECTS],
+      });
+    },
   });
 };
